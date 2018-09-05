9aa356a3bd350293cef6896e2352fb7cd10576cc
ravi <rafael.vitor@jusbrasil.com.br>
2018-09-04 21:07:13 -0300

    Fetch and render predictions

--- src/App.js
+++ src/App.js
@@ -2,17 +2,54 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { getPredictions } from './fetches';
+import { SSL_OP_ALLOW_UNSAFE_LEGACY_RENEGOTIATION } from 'constants';
+
 class App extends Component {
+  state = {
+    predictions: {},
+    loading: true,
+  }
+
+  async componentDidMount() {
+    const predictions = await getPredictions()
+
+    this.setState({
+      predictions: this.parse(predictions),
+      loading: false,
+    });
+  }
+
+  parse = (d) => (
+    d.services.filter(i => i.routeCode === '1114')
+  )
+
+  renderPredictions = () => (
+    this.state.predictions.map(i => (
+      <div key={i.routeCode}>
+        <div>
+          <div> { i.routeCode } </div>
+          <div> { i.routeMnemonic } </div>
+        </div>
+        {
+          i.vehicles.map(v => (
+            <div key={v.prediction}>
+              {Math.round(v.prediction / 60)}
+            </div>
+          ))
+        }
+      </div>
+    ))
+  )
+
   render() {
+    const { loading, predictions } = this.state;
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      { loading && <img src={logo} className="App-logo" alt="logo" /> }
+      { !loading &&
+        this.renderPredictions()
+      }
       </div>
     );
   }
---
+++ src/fetches.js
@@ -0,0 +1,24 @@
+const handleHttpStatus = res => {
+  if (res.status >= 200 && res.status < 300) {
+    return res.json();
+  }
+  throw res;
+};
+
+const createErrorHandler = defaultValue => res => {
+  console.error('request failed', res);
+  return defaultValue;
+};
+
+export const getPredictions = (locationId) => {
+  console.log(locationId)
+  return fetch('https://api.cittamobi.com.br/m3p/js/prediction/stop/1000002049', {
+    // credentials: 'same-origin',
+    method: 'get',
+    headers: {
+      'Content-Type': 'application/json',
+      'Accept': 'application/json',
+    },
+    // body: JSON.stringify(data),
+  }).then(handleHttpStatus).catch(createErrorHandler({}));
+}
